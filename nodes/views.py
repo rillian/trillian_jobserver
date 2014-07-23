1fcde796cd58d26f8d3fc6bd2a7b9e7ad21c5f42
Ralph Giles <giles@mozilla.com>
2014-07-23 14:12:31 -0700

    Use the 'render' shortcut method.

--- nodes/views.py
+++ nodes/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 from nodes.models import Node
 
 def index(request):
   node_list = Node.objects.all()[:10]
-  template = loader.get_template('nodes/index.html')
-  context = RequestContext(request, {
-    'up_node_list': node_list,
-  })
-  return HttpResponse(template.render(context))
+  context = {'node_list': node_list}
+  return render(request, 'nodes/index.html', context)
 
 def detail(request, node_id):
   return HttpResponse("Info on node %s." % node_id)
