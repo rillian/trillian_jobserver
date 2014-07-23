eb40381c5408d3c7035c686b45f0f5bf7f59057f
Ralph Giles <giles@thaumas.net>
2014-07-22 22:46:32 -0700

    Add stub per-node view.

--- node/urls.py
+++ node/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from node import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<node_id>\d+)/$', views.detail, name='detail'),
 )
--- node/views.py
+++ node/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello, this would be a list of nodes.")
+
+def detail(request, node_id):
+  return HttpResponse("Info on node %d." % int(node_id))
