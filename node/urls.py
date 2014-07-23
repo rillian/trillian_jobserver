2c7425c6767bd2ca2a2df02ca96ee5803c566623
Ralph Giles <giles@thaumas.net>
2014-07-22 23:04:12 -0700

    Add a template for the index view.
    It says 'available nodes' but doesn't actually filter by
    is_up().

--- node/urls.py
+++ node/urls.py
@@ -5,4 +5,5 @@ from node import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<node_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<node_id>\w+)/$', views.detail, name='detail'),
 )
--- node/views.py
+++ node/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from node.models import Node
 
 def index(request):
   nodes = Node.objects.all()[:10]
-  output = 'Known nodes: '
-  output += ', '.join([n.name for n in nodes])
-  return HttpResponse(output)
+  template = loader.get_template('nodes/index.html')
+  context = RequestContext(request, {
+    'up_node_list': nodes,
+  })
+  return HttpResponse(template.render(context))
 
 def detail(request, node_id):
-  return HttpResponse("Info on node %d." % int(node_id))
+  return HttpResponse("Info on node %s." % node_id)
