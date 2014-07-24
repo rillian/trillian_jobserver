6d02492469ca9979245af38780371a6d3cefc270
Ralph Giles <giles@thaumas.net>
2014-07-24 05:53:32 -0700

    Add a local settings overlay.

--- trillian_jobserver/settings.py
+++ trillian_jobserver/settings.py
@@ -81,3 +81,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Override these defaults with a local settings file.
+try:
+  from settings_local import *
+except ImportError:
+  pass
